22af6f8a002c6947df525da5508bac6276ecb146
bvegam <vegabryam40@gmail.com>
2024-12-10 08:27:51 -0500

    init project structure

---
+++ src/main/java/msa/innoit/prices/data/PricesDataApplication.java
@@ -0,0 +1,13 @@
+package msa.innoit.prices.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricesDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricesDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/msa/innoit/prices/data/PricesDataApplicationTests.java
@@ -0,0 +1,13 @@
+package msa.innoit.prices.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
